96770b8293ced301116224009fe34ae531f99f45
gggami <gggami@gmail.com>
2014-06-20 02:11:06 +0900

    update

--- bin/init_local_gitconfig.sh
+++ bin/init_local_gitconfig.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-git config user.name ""
-git config user.email ""
-   
+[ -z "$1" -o -z "$2" ] && exit 1
+
+git config user.name "$1"
+git config user.email "$2"
+
+echo "------ ./git/config ------"
+cat ./.git/config 
+echo "------ ./git/config ------"
